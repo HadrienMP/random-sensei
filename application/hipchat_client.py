d08d8939e36430aacdc73a4a1aad80e402747cb8
Hadrien Mens-Pellen <public@hadrienmp.fr>
2016-05-24 15:36:56 +0000

    Adds a hip chat client

--- application/hipchat_client.py
+++ application/hipchat_client.py
@@ -1,14 +1,17 @@
 from application import app
 import requests
 
-def room_members():
-    r = requests.get('http://darty-de.hipchat.com/v2/room/2761718/member?auth_token=' + app.config['HIPCHAT_TOKEN'])
-    
+def room_members(room):
+    r = _authenticated_get(app.config['HIPCHAT_ROOM_URL_FORMAT'].format(room=room))
     
     if r.status_code is 200:
         return [member['mention_name'] for member in r.json()['items']]
     else:
-        print(app.config['HIPCHAT_TOKEN'])
+        print(r.url)
         print(r.status_code)
         print(r.text)
         return []
+        
+        
+def _authenticated_get(url):
+    return requests.get(url + '?auth_token=' + app.config['HIPCHAT_TOKEN'])
--- tests/hipchat_client_test.py
+++ tests/hipchat_client_test.py
@@ -1,11 +1,18 @@
 from application import app
 from application import hipchat_client
 
+
 def test_should_return_room_members():
-    print app.config['HIPCHAT_TOKEN']
-    
     # WHEN
-    room_members = hipchat_client.room_members()
+    room_members = hipchat_client.room_members('2761495')
     
     # THEN
     assert room_members == ['HadrienMensPellen']
+
+
+def test_should_return_an_empty_list_for_an_invalid_room_id():
+    # WHEN
+    room_members = hipchat_client.room_members('-1')
+    
+    # THEN
+    assert room_members == list()
